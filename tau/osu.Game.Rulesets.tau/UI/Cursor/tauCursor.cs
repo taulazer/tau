00b8d61cb90021efde929e64ccaccb219ba129ae
Alten <altenyt@gmail.com>
2019-10-06 15:19:54 -0400

    improve mouse rotation

--- tau/osu.Game.Rulesets.tau/UI/Cursor/tauCursor.cs
+++ tau/osu.Game.Rulesets.tau/UI/Cursor/tauCursor.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Cursor;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Input.Events;
@@ -38,62 +38,90 @@ namespace osu.Game.Rulesets.tau.UI.Cursor
         {
             public DefaultCursor(float cs = 5f)
             {
-                RelativeSizeAxes = Axes.Both;
-
-                Anchor = Anchor.Centre;
                 Origin = Anchor.Centre;
+                Anchor = Anchor.Centre;
 
-                FillMode = FillMode.Fit;
-                FillAspectRatio = 1; // 1:1 Aspect Ratio.
+                RelativeSizeAxes = Axes.Both;
+                AddInternal(new GameplayCursor(cs));
 
-                InternalChildren = new Drawable[]
+                AddInternal(new CursorContainer
                 {
-                    new CircularContainer
-                    {
-                        RelativeSizeAxes = Axes.Both,
-                        Masking = true,
-                        Anchor = Anchor.Centre,
-                        Origin = Anchor.Centre,
-                        Size = new Vector2(0.75f),
-                        Children = new Drawable[]
-                        {
-                            new CircularProgress
-                            {
-                                RelativeSizeAxes = Axes.Both,
-                                Anchor = Anchor.Centre,
-                                Origin = Anchor.Centre,
-                                Current = new BindableDouble(convertValue(cs)),
-                                InnerRadius = 0.05f,
-                                Rotation = -360 * ((float)convertValue(cs) / 2)
-                            },
-                            new Box
-                            {
-                                Anchor = Anchor.TopCentre,
-                                Origin = Anchor.TopCentre,
-                                RelativeSizeAxes = Axes.Y,
-                                Size = new Vector2(5f, 0.5f),
-                            }
-                        }
-                    }
-                };
-
-                const double a = 1;
-                const double b = 10;
-                const double c = 0.2;
-                const double d = 0.005;
-
-                // Thank you AlFas for this code.
-                double convertValue(double value) => c + (d - c) * (value - a) / (b - a);
+                    RelativeSizeAxes = Axes.Both,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                });
             }
 
-            protected override bool OnMouseMove(MouseMoveEvent e)
+            private class GameplayCursor : CompositeDrawable
             {
-                var angle = Math.Atan2(OriginPosition.Y + Height / 2 - e.MousePosition.Y,
-                                OriginPosition.X + Width / 2 - e.MousePosition.X) * 180.0 / Math.PI - 90;
+                private readonly CircularContainer container;
+                private float cs;
 
-                Rotation = (float)angle;
+                public GameplayCursor(float cs)
+                {
+                    this.cs = cs;
+                    RelativeSizeAxes = Axes.Both;
 
-                return base.OnMouseMove(e);
+                    Anchor = Anchor.Centre;
+                    Origin = Anchor.Centre;
+
+                    FillMode = FillMode.Fit;
+                    FillAspectRatio = 1; // 1:1 Aspect Ratio.
+
+                    InternalChildren = new Drawable[]
+                    {
+                        container = new CircularContainer
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Masking = true,
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                            Size = new Vector2(0.75f),
+                            Children = new Drawable[]
+                            {
+                                new CircularProgress
+                                {
+                                    RelativeSizeAxes = Axes.Both,
+                                    Anchor = Anchor.Centre,
+                                    Origin = Anchor.Centre,
+                                    Current = new BindableDouble(convertValue(cs)),
+                                    InnerRadius = 0.05f,
+                                    Rotation = -360 * ((float)convertValue(cs) / 2)
+                                },
+                                new Box
+                                {
+                                    Anchor = Anchor.TopCentre,
+                                    Origin = Anchor.TopCentre,
+                                    RelativeSizeAxes = Axes.Y,
+                                    Size = new Vector2(5f, 0.5f),
+                                }
+                            }
+                        }
+                    };
+
+                    const double a = 1;
+                    const double b = 10;
+                    const double c = 0.2;
+                    const double d = 0.005;
+
+                    // Thank you AlFas for this code.
+                    double convertValue(double value) => c + (d - c) * (value - a) / (b - a);
+
+                    AddInternal(new Box
+                    {
+                        Position = AnchorPosition,
+                        Size = new Vector2(10)
+                    });
+                }
+
+                protected override bool OnMouseMove(MouseMoveEvent e)
+                {
+                    var angle = e.MousePosition.GetDegreesFromPosition(AnchorPosition);
+
+                    container.Rotation = angle;
+
+                    return base.OnMouseMove(e);
+                }
             }
         }
     }
