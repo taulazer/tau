effb3d6cab7489a851882625db36a1fce152c7ff
Coppertine <37494321+Coppertine@users.noreply.github.com>
2020-04-08 19:55:33 +1000

    Allow Judgement to all types of hitobjects.

--- osu.Game.Rulesets.tau/Objects/Drawables/DrawableTauJudgement.cs
+++ osu.Game.Rulesets.tau/Objects/Drawables/DrawableTauJudgement.cs
@@ -18,7 +18,7 @@ namespace osu.Game.Rulesets.Tau.Objects.Drawables
         private SkinnableSprite lighting;
         private Bindable<Color4> lightingColour;
 
-        public DrawableTauJudgement(JudgementResult result, DrawabletauBeatObject judgedObject)
+        public DrawableTauJudgement(JudgementResult result, DrawableTauHitObject judgedObject)
             : base(result, judgedObject)
         {
         }
