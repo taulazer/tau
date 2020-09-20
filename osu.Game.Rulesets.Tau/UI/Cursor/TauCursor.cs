9796d70f9f2f4cb734ad609b834454a2ca919339
Derrick Timmermans <derrick.timmermans@outlook.com>
2020-09-20 09:54:03 +0200

    Prevent stick ring from intersecting with paddle
    To improve the visual aspect

--- osu.Game.Rulesets.Tau/UI/Cursor/TauCursor.cs
+++ osu.Game.Rulesets.Tau/UI/Cursor/TauCursor.cs
@@ -140,7 +140,7 @@ namespace osu.Game.Rulesets.Tau.UI.Cursor
 
             protected override bool OnMouseMove(MouseMoveEvent e)
             {
-                circle.Y = -Math.Clamp(Vector2.Distance(AnchorPosition, e.MousePosition) / (DrawHeight), .015f, .485f);
+                circle.Y = -Math.Clamp(Vector2.Distance(AnchorPosition, e.MousePosition) / (DrawHeight), .015f, .45f);
                 bottomLine.Height = -circle.Y - .015f;
                 topLine.Height = .5f + circle.Y - .015f;
                 return base.OnMouseMove(e);
