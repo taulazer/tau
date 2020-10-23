73e7c03db86492d97a22dce8fe52e0c703a868a4
Noah <altenyt@gmail.com>
2020-10-23 10:25:01 -0400

    Add kiai test scene

---
+++ osu.Game.Rulesets.Tau.Tests/TestSceneKiai.cs
@@ -0,0 +1,44 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Testing;
+using osu.Game.Rulesets.Tau.UI;
+using osuTK;
+using osuTK.Graphics;
+
+namespace osu.Game.Rulesets.Tau.Tests
+{
+    public class TestSceneKiai : TestScene
+    {
+        public TestSceneKiai()
+        {
+            AddStep("Hit Single", () => testSingle());
+            AddStep("Hit Stream", testMultiple);
+
+            AddStep("Hit hard beat", () => Add(new KiaiHitExplosion(Color4.White, true)
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Size = new Vector2(0.5f)
+            }));
+        }
+
+        private void testSingle(float angle = 0)
+        {
+            Add(new KiaiHitExplosion(Color4.White)
+            {
+                Position = Extensions.GetCircularPosition(0.15f, angle),
+                Angle = angle,
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+            });
+        }
+
+        private void testMultiple()
+        {
+            for (int i = 0; i <= 1000; i += 100)
+            {
+                var angle = i / 5f;
+                Scheduler.AddDelayed(() => testSingle(angle), i);
+            }
+        }
+    }
+}
