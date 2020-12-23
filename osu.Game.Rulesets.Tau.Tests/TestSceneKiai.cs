f80da10547712888d85e5ddb2ff40f4a160852d6
Noah <altenyt@gmail.com>
2020-12-23 10:25:03 -0500

    Make kiai effects skinnable

--- osu.Game.Rulesets.Tau.Tests/TestSceneKiai.cs
+++ osu.Game.Rulesets.Tau.Tests/TestSceneKiai.cs
@@ -1,44 +1,71 @@
 ﻿using osu.Framework.Graphics;
-using osu.Framework.Testing;
+using osu.Framework.Graphics.Containers;
 using osu.Game.Rulesets.Tau.UI;
 using osuTK;
 using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Tau.Tests
 {
-    public class TestSceneKiai : TestScene
+    public class TestSceneKiai : TauSkinnableTestScene
     {
         public TestSceneKiai()
         {
-            AddStep("Hit Single", () => testSingle());
-            AddStep("Hit Stream", testMultiple);
+            AddStep("Hit Single", () => SetContents(() => testSingle()));
+            AddStep("Hit Stream", () => SetContents(testMultiple));
 
-            AddStep("Hit hard beat", () => Add(new KiaiHitExplosion(Color4.White, true)
+            AddStep("Hit hard beat", () => SetContents(() => new Container
             {
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
-                Size = new Vector2(0.5f)
+                RelativeSizeAxes = Axes.Both,
+                FillAspectRatio = 1,
+                FillMode = FillMode.Fit,
+                Children = new Drawable[]
+                {
+                    new KiaiHitExplosion(Color4.White, true)
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Size = new Vector2(0.5f)
+                    }
+                }
             }));
         }
 
-        private void testSingle(float angle = 0)
+        private Drawable testSingle(float angle = 0)
         {
-            Add(new KiaiHitExplosion(Color4.White)
+            return new Container
             {
-                Position = Extensions.GetCircularPosition(0.15f, angle),
-                Angle = angle,
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
-            });
+                RelativeSizeAxes = Axes.Both,
+                FillAspectRatio = 1,
+                FillMode = FillMode.Fit,
+                Children = new Drawable[]
+                {
+                    new KiaiHitExplosion(Color4.White)
+                    {
+                        Position = Extensions.GetCircularPosition(0.15f, angle),
+                        Angle = angle,
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                    }
+                }
+            };
         }
 
-        private void testMultiple()
+        private Drawable testMultiple()
         {
+            var container = new Container
+            {
+                RelativeSizeAxes = Axes.Both,
+                FillAspectRatio = 1,
+                FillMode = FillMode.Fit,
+            };
+
             for (int i = 0; i <= 1000; i += 100)
             {
                 var angle = i / 5f;
-                Scheduler.AddDelayed(() => testSingle(angle), i);
+                Scheduler.AddDelayed(() => container.Add(testSingle(angle)), i);
             }
+
+            return container;
         }
     }
 }
--- osu.Game.Rulesets.Tau/UI/KiaiHitExplosion.cs
+++ osu.Game.Rulesets.Tau/UI/KiaiHitExplosion.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osu.Framework.Timing;
+using osu.Framework.Utils;
+using osu.Game.Rulesets.Tau.Skinning;
+using osu.Game.Skinning;
 using osuTK;
 using osuTK.Graphics;
 
@@ -11,8 +18,9 @@ namespace osu.Game.Rulesets.Tau.UI
     public class KiaiHitExplosion : CompositeDrawable
     {
         public override bool RemoveWhenNotAlive => true;
-        private readonly List<Drawable> particles;
         private readonly bool circular;
+        private readonly Color4 colour;
+        private readonly int particleAmount;
 
         /// <summary>
         /// Used whenever circular isn't set to True.
@@ -21,80 +29,150 @@ namespace osu.Game.Rulesets.Tau.UI
 
         public KiaiHitExplosion(Color4 colour, bool circular = false, int particleAmount = 10)
         {
+            this.colour = colour;
             this.circular = circular;
-            var rng = new Random();
-            particles = new List<Drawable>();
+            this.particleAmount = particleAmount;
 
             RelativePositionAxes = Axes.Both;
             RelativeSizeAxes = Axes.Both;
-
-            if (circular)
-            {
-                for (int i = 0; i < particleAmount; i++)
-                {
-                    particles.Add(new Box
-                    {
-                        RelativePositionAxes = Axes.Both,
-                        Position = Extensions.GetCircularPosition(((float)(rng.NextDouble() * 0.15f) * 0.15f) + 0.5f, (float)rng.NextDouble() * 360f),
-                        Rotation = (float)rng.NextDouble() * 360f,
-                        Anchor = Anchor.Centre,
-                        Origin = Anchor.BottomCentre,
-                        Size = new Vector2(rng.Next(1, 15)),
-                        Colour = colour
-                    });
-                }
-            }
-            else
-            {
-                for (int i = 0; i < particleAmount; i++)
-                {
-                    particles.Add(new Box
-                    {
-                        RelativePositionAxes = Axes.Both,
-                        Position = Extensions.GetCircularPosition((float)(rng.NextDouble() * 0.15f) * 0.15f, ((float)rng.NextDouble() / 10 * 10) + (Angle - 20)),
-                        Rotation = (float)rng.NextDouble() * 360f,
-                        Anchor = Anchor.Centre,
-                        Origin = Anchor.BottomCentre,
-                        Size = new Vector2(rng.Next(1, 15)),
-                        Colour = colour
-                    });
-                }
-            }
-
-            AddRangeInternal(particles.ToArray());
         }
 
-        protected override void LoadComplete()
+        [BackgroundDependencyLoader]
+        private void load(ISkinSource skin)
         {
-            base.LoadComplete();
+            var animated = skin.GetConfig<TauSkinConfiguration, bool>(TauSkinConfiguration.AnimateKiai)?.Value ?? false;
 
-            foreach (var particle in particles)
+            if (animated)
             {
-                const double duration = 1500;
+                var container = new Container
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Child = skin.GetAnimation("kiai" + (circular ? "-big" : string.Empty), true, false, true).With(a =>
+                    {
+                        a.FillMode = FillMode.Fit;
+                        a.RelativeSizeAxes = Axes.Both;
+                    }),
+                    Clock = new FramedClock()
+                };
+
+                if (circular)
+                    container.RelativeSizeAxes = Axes.Both;
+                else
+                    container.Size = new Vector2(RNG.NextSingle(25, 25));
+
+                container.Delay(1500).Expire();
+
+                AddInternal(container);
+            }
+            else
+                AddInternal(new KiaiHitExplosionEmitter(colour, circular, particleAmount) { Angle = Angle });
+        }
+
+        private class KiaiHitExplosionEmitter : CompositeDrawable
+        {
+            public override bool RemoveWhenNotAlive => true;
+            private readonly List<Drawable> particles;
+            private readonly bool circular;
+            private readonly Color4 colour;
+            private readonly int particleAmount;
+            public float Angle;
+
+            public KiaiHitExplosionEmitter(Color4 colour, bool circular, int particleAmount)
+            {
+                this.colour = colour;
+                this.circular = circular;
+                this.particleAmount = particleAmount;
+                particles = new List<Drawable>();
+
+                RelativePositionAxes = Axes.Both;
+                RelativeSizeAxes = Axes.Both;
+            }
+
+            [BackgroundDependencyLoader]
+            private void load(ISkinSource skin)
+            {
                 var rng = new Random();
+                Texture kiai;
+                Texture kiaiBig;
+
+                if ((kiai = skin.GetTexture("kiai")) == null)
+                    kiai = Texture.WhitePixel;
+
+                if ((kiaiBig = skin.GetTexture("kiai-big")) == null)
+                    kiaiBig = Texture.WhitePixel;
 
                 if (circular)
                 {
-                    particle.MoveTo(Extensions.GetCircularPosition(((float)(rng.NextDouble() * 0.15f) * 2f) + 0.5f, Vector2.Zero.GetDegreesFromPosition(particle.Position)), duration, Easing.OutQuint)
-                            .ScaleTo(new Vector2(rng.Next(1, 2)), duration, Easing.OutQuint)
-                            .FadeOut(duration, Easing.OutQuint);
+                    for (int i = 0; i < particleAmount; i++)
+                    {
+                        particles.Add(new Sprite
+                        {
+                            RelativePositionAxes = Axes.Both,
+                            Position = Extensions.GetCircularPosition(((float)(rng.NextDouble() * 0.15f) * 0.15f) + 0.5f, (float)rng.NextDouble() * 360f),
+                            Rotation = (float)rng.NextDouble() * 360f,
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.BottomCentre,
+                            Size = new Vector2(rng.Next(1, 15)),
+                            Colour = colour,
+                            Texture = kiaiBig
+                        });
+                    }
                 }
                 else
                 {
-                    particle.MoveTo(Extensions.GetCircularPosition((float)(rng.NextDouble() * 0.15f) * 1f, randomBetween(Angle - 40, Angle + 40)), duration, Easing.OutQuint)
-                            .ResizeTo(new Vector2(rng.Next(0, 5)), duration, Easing.OutQuint)
-                            .FadeOut(duration, Easing.OutQuint);
-
-                    float randomBetween(float smallNumber, float bigNumber)
+                    for (int i = 0; i < particleAmount; i++)
                     {
-                        float diff = bigNumber - smallNumber;
-
-                        return ((float)rng.NextDouble() * diff) + smallNumber;
+                        particles.Add(new Sprite
+                        {
+                            RelativePositionAxes = Axes.Both,
+                            Position = Extensions.GetCircularPosition((float)(rng.NextDouble() * 0.15f) * 0.15f, ((float)rng.NextDouble() / 10 * 10) + (Angle - 20)),
+                            Rotation = (float)rng.NextDouble() * 360f,
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.BottomCentre,
+                            Size = new Vector2(rng.Next(1, 15)),
+                            Colour = colour,
+                            Texture = kiai
+                        });
                     }
                 }
 
-                particle.Expire(true);
-                this.Delay(duration).Expire(true);
+                AddRangeInternal(particles.ToArray());
+            }
+
+            protected override void LoadComplete()
+            {
+                base.LoadComplete();
+
+                foreach (var particle in particles)
+                {
+                    const double duration = 1500;
+                    var rng = new Random();
+
+                    if (circular)
+                    {
+                        particle.MoveTo(Extensions.GetCircularPosition(((float)(rng.NextDouble() * 0.15f) * 2f) + 0.5f, Vector2.Zero.GetDegreesFromPosition(particle.Position)), duration,
+                                     Easing.OutQuint)
+                                .ScaleTo(new Vector2(rng.Next(1, 2)), duration, Easing.OutQuint)
+                                .FadeOut(duration, Easing.OutQuint);
+                    }
+                    else
+                    {
+                        particle.MoveTo(Extensions.GetCircularPosition((float)(rng.NextDouble() * 0.15f) * 1f, randomBetween(Angle - 40, Angle + 40)), duration, Easing.OutQuint)
+                                .ResizeTo(new Vector2(rng.Next(0, 5)), duration, Easing.OutQuint)
+                                .FadeOut(duration, Easing.OutQuint);
+
+                        float randomBetween(float smallNumber, float bigNumber)
+                        {
+                            float diff = bigNumber - smallNumber;
+
+                            return ((float)rng.NextDouble() * diff) + smallNumber;
+                        }
+                    }
+
+                    particle.Expire(true);
+                    this.Delay(duration).Expire(true);
+                }
             }
         }
     }
