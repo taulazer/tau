5283ba1cd01a08c479fbdf745c512285021a05b5
Noah <altenyt@gmail.com>
2020-12-23 10:28:40 -0500

    Final cleanup

--- osu.Game.Rulesets.Tau/UI/KiaiHitExplosion.cs
+++ osu.Game.Rulesets.Tau/UI/KiaiHitExplosion.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
 using osu.Framework.Timing;
