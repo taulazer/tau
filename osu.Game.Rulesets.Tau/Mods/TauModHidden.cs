18f147d179537324eea71fdcd4e9e3d1dbd3c56b
Noah <altenyt@gmail.com>
2021-08-03 12:30:45 -0400

    Adjust fade-out time for Sliders.

--- osu.Game.Rulesets.Tau/Mods/TauModHidden.cs
+++ osu.Game.Rulesets.Tau/Mods/TauModHidden.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using osu.Framework.Graphics;
 using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.Tau.Objects;
 using osu.Game.Rulesets.Tau.Objects.Drawables;
 
 namespace osu.Game.Rulesets.Tau.Mods
@@ -17,7 +19,7 @@ namespace osu.Game.Rulesets.Tau.Mods
         private const double fade_in_duration_multiplier = 0.4;
         private const double fade_out_duration_multiplier = 0.3;
 
-        public override bool HasImplementation => false;
+        protected override bool IsFirstAdjustableObject(HitObject hitObject) => !(hitObject is HardBeat);
 
         public override void ApplyToDrawableHitObjects(IEnumerable<DrawableHitObject> drawables)
         {
@@ -46,18 +48,29 @@ namespace osu.Game.Rulesets.Tau.Mods
             var h = d.HitObject;
 
             var fadeOutStartTime = h.StartTime - h.TimePreempt + h.TimeFadeIn;
-            var fadeOutDuration = h.TimePreempt * fade_out_duration_multiplier;
+            double fadeOutDuration = 0;
 
             // future proofing yet again.
             switch (drawable)
             {
+                case DrawableSlider slider:
+                    fadeOutDuration = h.TimePreempt + slider.HitObject.Duration * fade_out_duration_multiplier;
+
+                    using (drawable.BeginAbsoluteSequence(fadeOutStartTime))
+                        slider.FadeOut(fadeOutDuration);
+
+                    break;
+
                 case DrawableTauHitObject beat:
+                    fadeOutDuration = h.TimePreempt * fade_out_duration_multiplier;
+
                     using (drawable.BeginAbsoluteSequence(fadeOutStartTime, true))
                         beat.FadeOut(fadeOutDuration);
 
                     break;
             }
         }
+
         protected override void ApplyIncreasedVisibilityState(DrawableHitObject hitObject, ArmedState state)
         {
         }
