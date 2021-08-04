cd72923dd1c5ab5e37622c666858dc3b862e80cf
Noah <altenyt@gmail.com>
2021-08-04 13:59:02 -0400

    Remove unnecessary lines

--- osu.Game.Rulesets.Tau/Objects/Drawables/DrawableSliderHead.cs
+++ osu.Game.Rulesets.Tau/Objects/Drawables/DrawableSliderHead.cs
@@ -1,16 +1,8 @@
-﻿using JetBrains.Annotations;
-using osu.Framework.Allocation;
-
-namespace osu.Game.Rulesets.Tau.Objects.Drawables
+﻿namespace osu.Game.Rulesets.Tau.Objects.Drawables
 {
     public class DrawableSliderHead : DrawableBeat
     {
         public new SliderHeadBeat HitObject => (SliderHeadBeat)base.HitObject;
-
-        [CanBeNull]
-        public Slider Slider => DrawableSlider?.HitObject;
-
-        protected DrawableSlider DrawableSlider => (DrawableSlider)ParentHitObject;
 
         public DrawableSliderHead()
         {
@@ -19,18 +11,6 @@ namespace osu.Game.Rulesets.Tau.Objects.Drawables
         public DrawableSliderHead(SliderHeadBeat obj)
             : base(obj)
         {
-        }
-
-        [BackgroundDependencyLoader]
-        private void load()
-        {
-            Slider?.Nodes.BindCollectionChanged((_, __) => updateAngle(), true);
-        }
-
-        private void updateAngle()
-        {
-            if (Slider != null)
-                HitObject.Angle = Slider.Nodes[0].Angle;
         }
     }
 }
