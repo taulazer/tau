61c64d159547618601513e043186607fc6085dca
Derrick Timmermans <derrick.timmermans@outlook.com>
2021-09-16 20:37:15 +0200

    Use AddGame in TestSceneOsuGame

--- osu.Game.Rulesets.Tau.Tests/TestSceneOsuGame.cs
+++ osu.Game.Rulesets.Tau.Tests/TestSceneOsuGame.cs
@@ -1,6 +1,4 @@
 using osu.Framework.Allocation;
-using osu.Framework.Graphics;
-using osu.Framework.Platform;
 using osu.Game.Tests.Visual;
 
 namespace osu.Game.Rulesets.Tau.Tests
@@ -8,15 +6,9 @@ namespace osu.Game.Rulesets.Tau.Tests
     public class TestSceneOsuGame : OsuTestScene
     {
         [BackgroundDependencyLoader]
-        private void load(GameHost host, OsuGameBase gameBase)
+        private void load()
         {
-            OsuGame game = new OsuGame();
-            game.SetHost(host);
-
-            Children = new Drawable[]
-            {
-                game
-            };
+            AddGame(new OsuGame());
         }
     }
 }
