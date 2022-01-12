80f14ab7f3aea89254e99f882ce1485b8109c348
naoei <altenyt@gmail.com>
2022-01-12 03:12:51 -0500

    Add extensions

---
+++ osu.Game.Rulesets.Tau/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using osuTK;
+
+namespace osu.Game.Rulesets.Tau
+{
+    public static class Extensions
+    {
+        public static Vector2 GetCircularPosition(float distance, float angle)
+            => new Vector2(
+                -(distance * MathF.Cos((angle + 90f) * (MathF.PI / 180))),
+                -(distance * MathF.Sin((angle + 90f) * (MathF.PI / 180))));
+
+        public static float GetHitObjectAngle(this Vector2 target)
+        {
+            var offset = new Vector2(256, 192) - target;
+            var temp = MathHelper.RadiansToDegrees(MathF.Atan2(offset.X, -offset.Y)) - 180;
+            NormalizeAngle(ref temp);
+
+            return temp;
+        }
+
+        public static void NormalizeAngle(this ref float angle)
+        {
+            if (angle < 0) angle += 360;
+            if (angle >= 360) angle %= 360;
+        }
+
+        public static float Remap(this float value, float x1, float x2, float y1, float y2)
+        {
+            var m = (y2 - y1) / (x2 - x1);
+            var c = y1 - m * x1;
+
+            return m * value + c;
+        }
+    }
+}
