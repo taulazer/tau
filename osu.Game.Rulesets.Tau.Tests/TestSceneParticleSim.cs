fcfc8aa6a13512d0e75e5c37ca9adcbbd32300e9
Flutterish <CharmindYT@gmail.com>
2022-03-30 22:51:44 +0200

    create basic (unoptimized) particle simulation with SPH

---
+++ osu.Game.Rulesets.Tau.Tests/TestSceneParticleSim.cs
@@ -0,0 +1,432 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Utils;
+using osu.Game.Rulesets.Objects.Types;
+using osu.Game.Rulesets.Tau.Physics;
+using osu.Game.Tests.Visual;
+using osuTK;
+using System;
+using System.Collections.Generic;
+
+namespace osu.Game.Rulesets.Tau.Tests
+{
+    public class TestSceneParticleSim : OsuTestScene
+    {
+        List<Particle> particles = new();
+        void Add (Particle particle) {
+            particle.Depth = -1;
+            particles.Add(particle);
+            base.Add(particle);
+
+            distanceHash.Add(particle.X, particle.Y, particle);
+        }
+
+        const float FIELD_SCALE = 50;
+        DistanceHash<Particle> distanceHash = new(radius: FIELD_SCALE);
+
+        public TestSceneParticleSim () {
+            var rng = new Random(122345);
+
+            float next (float min, float max) {
+                return min + rng.NextSingle() * ( max - min );
+            }
+
+            for ( int i = 0; i < 1000; i++ ) {
+                Add( new Particle {
+                    Anchor = Anchor.Centre,
+                    Position = new( next( -200, 200 ), next( -200, 200 ) ),
+                    Velocity = new Vector2( next( -1, 1 ), next( -1, 1 ) ).Normalized() * 10
+                } );
+            }
+
+            //for ( int y = 0; y < 10; y++ ) {
+            //    for ( int x = -3; x <= 3; x++ ) {
+            //        Add( new Particle {
+            //            Anchor = Anchor.Centre,
+            //            Position = new( x * 5 + 30, y * 15 + 100 ),
+            //            Velocity = new Vector2( 0, -40 )
+            //        } );
+            //    }
+            //}
+
+            Add( new Particle {
+                Anchor = Anchor.Centre,
+                Mass = 100
+            } );
+
+            //for ( int y = -4; y < 4; y++ ) {
+            //    for ( int x = -4; x <= 4; x++ ) {
+            //        Add( new Particle {
+            //            Anchor = Anchor.Centre,
+            //            Position = new( x * 20, y * 20 )
+            //        } );
+            //    }
+            //}
+
+            // pressure
+            //createScalarField( 20, 20, 18, x => x.Mass, Field.KernelBell() );
+            // negative pressure gradient
+            //createVectorField( 30, 30, 12, x => x.Mass, Field.Gradient(Field.KernelSpike()) );
+
+            // viscosity
+            // velocity
+            createVectorField( 15, 15, 18 * 4 / 3, x => x.Velocity, Field.KernelBell() );
+            // laplacian
+            //createVectorField( 30, 30, 12, x => x.Velocity, Field.Laplacian( Field.KernelAsymptote()) );
+
+            // color
+            //createScalarField( 20, 20, 18, x => x.Volume, Field.KernelBell() );
+            // normal into fluid
+            //var normal = Field.Gradient( Field.KernelBell() );
+            //createVectorField( 20, 20, 18, x => x.Volume, normal );
+            // curvature
+            //var lapl = Field.Laplacian( Field.KernelBell() );
+            //Field.ScalarFn curvature = p => {
+            //    var n = normal(p).Length;
+            //    if (n == 0)
+            //        return 0;
+            //    else
+            //        return -lapl(p)/n;
+            //};
+            //createScalarField( 10, 10, 36, x => x.Volume, lapl );
+            //createScalarField( 30, 30, 12, x => x.Volume, curvature );
+            // traction
+            //Field.VectorFn traction = p => {
+            //    var v = normal( p );
+            //    var n = v.Length;
+            //    if ( n == 0 )
+            //        return Vector2.Zero;
+            //    else
+            //        return -lapl( p ) / n * v / n;
+            //};
+            //createVectorField( 30, 30, 12, x => x.Volume, traction );
+            // force
+            //createVectorField( 10, 10, 36, x => x.Volume, r => {
+            //    var n = normal( r );
+            //    if (n == Vector2.Zero)
+            //        return Vector2.Zero;
+
+            //    return -lapl( r ) * n.Normalized();
+            //} );
+        }
+
+        Drawable createField<T, Tdrawable, Tdata> ( int w, int h, float res,
+            Func<Vector2, T> selector, Func<Memory<T>, Tdata> analizer, Func<Vector2, Tdrawable> visualizer, Action<T, Tdrawable, Tdata> updater
+        ) where Tdrawable : Drawable {
+            Container<Tdrawable> container = new() {
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre,
+                AutoSizeAxes = Axes.Both
+            };
+            Add(container);
+
+            for ( int x = -w; x <= h; x++ ) {
+                for ( int y = -h; y <= h; y++ ) {
+                    container.Add( visualizer( new Vector2( x, y ) * res ) );
+                }
+            }
+
+            var l = ( 2 * w + 1 ) * ( 2 * h + 1 );
+            container.OnUpdate += _ => {
+                using var _1 = ArrayPool<T>.Rent( l, out var data );
+                int i = 0;
+
+                foreach ( var d in container ) {
+                    data[i++] = selector(d.Position);
+                }
+
+                var settings = analizer(data.AsMemory(0, l));
+
+                i = 0;
+                foreach ( var d in container ) {
+                    updater( data[i++], d, settings );
+                }
+            };
+
+            return container;
+        }
+
+        private static float max<T> ( Memory<T> memory, Func<T, float> selector ) {
+            var max = float.NegativeInfinity;
+            foreach ( var k in memory.Span ) {
+                var n = selector(k);
+                if ( max < n )
+                    max = n;
+            }
+            return max;
+        }
+        private static float max ( Memory<float> memory ) {
+            var max = float.NegativeInfinity;
+            foreach ( var n in memory.Span ) {
+                if ( max < n )
+                    max = n;
+            }
+            return max;
+        }
+        private static float min<T> ( Memory<T> memory, Func<T, float> selector ) {
+            var min = float.PositiveInfinity;
+            foreach ( var k in memory.Span ) {
+                var n = selector(k);
+                if ( min > n )
+                    min = n;
+            }
+            return min;
+        }
+        private static float min ( Memory<float> memory ) {
+            var min = float.PositiveInfinity;
+            foreach ( var n in memory.Span ) {
+                if ( min > n )
+                    min = n;
+            }
+            return min;
+        }
+
+        Drawable createScalarField ( int w, int h, float res, Func<Particle, float> selector, Field.ScalarFn kernel, float? from = null, float? to = null ) {
+            return createField( w, h, res, pos => {
+                return ParticleField<Particle>.FieldAt( pos, distanceHash.GetClose( pos, FIELD_SCALE ), selector, kernel, FIELD_SCALE );
+            }, data => {
+                return (from is null ? min(data) : from.Value, to is null ? max(data) : to.Value);
+            }, pos => new Box {
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre,
+                Position = pos,
+                Size = new( res )
+            }, (v, d, range) => {
+                d.Colour = Interpolation.ValueAt( v, Colour4.Blue, Colour4.Red, range.Item1, range.Item2 );
+            } );
+        }
+
+        Drawable createVectorField ( int w, int h, float res, Func<Particle, float> selector, Field.VectorFn gradient ) {
+            return createField( w, h, res, pos => {
+                return ParticleField<Particle>.FieldAt( pos, distanceHash.GetClose( pos, FIELD_SCALE ), selector, gradient, FIELD_SCALE );
+            }, data => {
+                return (min(data, x => x.Length), max(data, x => x.Length));
+            }, pos => new Box {
+                Origin = Anchor.CentreLeft,
+                Anchor = Anchor.Centre,
+                Position = pos
+            }, (v, d, range) => {
+                d.Size = range.Item1 == range.Item2 ? Vector2.Zero : new( res * ( v.Length - range.Item1 ) / (range.Item2 - range.Item1) / 2, res / 8 );
+                d.Rotation = MathF.Atan2( v.Y, v.X ) / MathF.PI * 180;
+                d.Colour = Interpolation.ValueAt( v.Length, Colour4.Blue, Colour4.Green, range.Item1, range.Item2 );
+            } );
+        }
+
+        Drawable createVectorField ( int w, int h, float res, Func<Particle, Vector2> selector, Field.ScalarFn kernel ) {
+            return createField( w, h, res, pos => {
+                return ParticleField<Particle>.FieldAt( pos, distanceHash.GetClose( pos, FIELD_SCALE ), selector, kernel, FIELD_SCALE );
+            }, data => {
+                return (min( data, x => x.Length ), max( data, x => x.Length ));
+            }, pos => new Box {
+                Origin = Anchor.CentreLeft,
+                Anchor = Anchor.Centre,
+                Position = pos
+            }, ( v, d, range ) => {
+                d.Size = range.Item1 == range.Item2 ? Vector2.Zero : new( res * ( v.Length - range.Item1 ) / ( range.Item2 - range.Item1 ) / 2, res / 8 );
+                d.Rotation = MathF.Atan2( v.Y, v.X ) / MathF.PI * 180;
+                d.Colour = Interpolation.ValueAt( v.Length, Colour4.Blue, Colour4.Green, range.Item1, range.Item2 );
+            } );
+        }
+
+        protected override void Update () {
+            base.Update();
+
+            Vector2 pressureGradientField ( Particle i ) {
+                return ParticleField<Particle>.FieldAt(
+                    i.Position, distanceHash.GetClose( i.Position, FIELD_SCALE ),
+                    j => j.Mass * ( j.Density + i.Density ) / 2 / j.Density,
+                    Field.Gradient( Field.KernelSpike() ),
+                    FIELD_SCALE
+                );
+            }
+
+            float densityField ( Particle i ) {
+                return ParticleField<Particle>.FieldAt(
+                    i.Position, distanceHash.GetClose( i.Position, FIELD_SCALE ),
+                    j => j.Mass,
+                    Field.KernelSpike(),
+                    FIELD_SCALE
+                );
+            }
+
+            Vector2 viscosityField ( Particle i ) {
+                return ParticleField<Particle>.FieldAt(
+                    i.Position, distanceHash.GetClose( i.Position, FIELD_SCALE ),
+                    j => j.Mass * ( j.Velocity - i.Velocity ) / j.Density,
+                    Field.KernelBell(),
+                    FIELD_SCALE
+                );
+            }
+
+            Vector2 colorGradient ( Particle i ) {
+                return ParticleField<Particle>.FieldAt(
+                    i.Position, distanceHash.GetClose( i.Position, FIELD_SCALE ),
+                    j => j.Volume,
+                    Field.Gradient( Field.KernelBell() ),
+                    FIELD_SCALE
+                );
+            }
+            float colorLaplacian ( Particle i ) {
+                return ParticleField<Particle>.FieldAt(
+                    i.Position, distanceHash.GetClose( i.Position, FIELD_SCALE ),
+                    j => j.Volume,
+                    Field.Laplacian( Field.KernelBell() ),
+                    FIELD_SCALE
+                );
+            }
+            Vector2 sufraceTensionForce ( Particle i ) {
+                var n = colorGradient( i );
+                if ( n == Vector2.Zero )
+                    return Vector2.Zero;
+
+                return -colorLaplacian( i ) * n.Normalized();
+            }
+
+            Vector2 forceField ( Particle p ) {
+                return -pressureGradientField( p ) + viscosityField( p ) /*+ sufraceTensionForce( p ) / 6*/;
+            }
+
+            Vector2 accelerationField ( Particle p ) {
+                // acceleration is force / mass
+                return forceField( p ) / densityField( p );
+            }
+
+            var delta = MathF.Min( (float)Time.Elapsed, 100 ) / 1000 * 14;
+            using ( ArrayPool<Vector2>.Rent( particles.Count, out var acceleration ) ) {
+                int i = 0;
+                foreach ( var p in particles ) {
+                    acceleration[ i++ ] = accelerationField( p );
+                }
+
+                i = 0;
+                foreach ( var p in particles ) {
+                    p.Velocity += acceleration[ i++ ] * delta;
+                    p.Position += p.Velocity * delta;
+
+                    if ( p.X < -360 ) {
+                        p.X = -360;
+                        p.Velocity.X *= -1;
+                    }
+                    if ( p.X > 360 ) {
+                        p.X = 360;
+                        p.Velocity.X *= -1;
+                    }
+                    if ( p.Y < -360 ) {
+                        p.Y = -360;
+                        p.Velocity.Y *= -1;
+                    }
+                    if ( p.Y > 360 ) {
+                        p.Y = 360;
+                        p.Velocity.Y *= -1;
+                    }
+                }
+            }
+
+            distanceHash.Clear();
+            foreach (var i in particles) {
+                distanceHash.Add(i.X, i.Y, i);
+            }
+        }
+
+        private class Particle : Circle, IHasPosition {
+            public Vector2 Velocity;
+
+            private float mass = 1;
+            private float density = 1;
+            public float Mass {
+                get => mass;
+                set {
+                    mass = value;
+                    Size = new( MathF.Sqrt( mass / density ) * 4 );
+                }
+            }
+            public float Density {
+                get => density;
+                set {
+                    density = value;
+                    Size = new( MathF.Sqrt( mass / density ) * 4 );
+                }
+            }
+            public float Volume => mass / density;
+
+            public Particle () {
+                Origin = Anchor.Centre;
+                Mass = 1;
+            }
+        }
+    }
+
+    public static class ArrayPool<T> {
+        public static IDisposable Rent (int length, out T[] arr) {
+            arr = System.Buffers.ArrayPool<T>.Shared.Rent( length );
+            return new ReturnArray(arr);
+        }
+
+        private struct ReturnArray : IDisposable {
+            T[] arr;
+
+            public ReturnArray ( T[] arr ) {
+                this.arr = arr;
+            }
+
+            public void Dispose () {
+                System.Buffers.ArrayPool<T>.Shared.Return( arr );
+            }
+        }
+    }
+
+    public class DistanceHash<T> {
+        Stack<List<T>> listPool = new();
+        private Dictionary<(int x, int y), List<T>> hash = new();
+        public readonly float Radius;
+
+        public DistanceHash ( float radius ) {
+            Radius = radius;
+        }
+
+        private List<T> nextList () {
+            return listPool.TryPop( out var l ) ? l : new();
+        }
+        public void Clear () {
+            foreach (var l in hash.Values) {
+                l.Clear();
+                listPool.Push(l);
+            }
+            hash.Clear();
+        }
+
+        public void Add ( Vector2 pos, T value ) {
+            Add( pos.X, pos.Y, value );
+        }
+        public void Add (float x, float y, T value) {
+            int x2 = (int)MathF.Round( x / Radius );
+            int y2 = (int)MathF.Round( y / Radius );
+
+            if (!hash.TryGetValue((x2, y2), out var list))
+                hash.Add((x2, y2), list = nextList());
+
+            list.Add(value);
+        }
+
+        public IEnumerable<T> GetClose ( Vector2 pos, float distance ) {
+            return GetClose( pos.X, pos.Y, distance );
+        }
+        public IEnumerable<T> GetClose ( float x, float y, float distance ) {
+            int xFrom = (int)MathF.Round( ( x - distance ) / Radius );
+            int xTo = (int)MathF.Round( ( x + distance ) / Radius );
+            int yFrom = (int)MathF.Round( ( y - distance ) / Radius );
+            int yTo = (int)MathF.Round( ( y + distance ) / Radius );
+
+            for ( int i = xFrom; i <= xTo; i++ ) {
+                for ( int j = yFrom; j <= yTo; j++ ) {
+                    if ( hash.TryGetValue((i, j), out var list) ) {
+                        foreach ( var k in list ) {
+                            yield return k;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Tau/Physics/Field.cs
@@ -0,0 +1,199 @@
+﻿using osu.Game.Rulesets.Objects.Types;
+using osuTK;
+using System;
+using System.Collections.Generic;
+
+namespace osu.Game.Rulesets.Tau.Physics {
+    // https://www.plymouth.ac.uk/uploads/production/document/path/3/3744/PlymouthUniversity_MathsandStats_the_Laplacian.pdf
+    // https://activecalculus.org/vector/S_Vector_VectorFields.html
+    public static class Field {
+        public delegate float ScalarFn ( Vector2 p );
+        public delegate Vector2 VectorFn ( Vector2 p );
+
+        /// <summary>
+        /// Denoted ∇f(x,y) where f: (R, R) -> R.
+        /// Computes the partial derivative along the 2 axis.
+        /// </summary>
+        /// <remarks>
+        /// This is equivalent to (∂f/∂x, ∂f/∂y)
+        /// </remarks>
+        /// <param name="s">The numerical precision of the derivatives</param>
+        /// <returns>F(x, y) where F: (R, R) -> (R, R)</returns>
+        public static VectorFn Gradient ( ScalarFn fn, float s = 0.001f ) {
+            var sInv = 1 / s / 2;
+            return p => {
+                var k = fn(p);
+                var dx = (fn(p + new Vector2(s, 0)) - fn(p - new Vector2(s, 0))) * sInv;
+                var dy = (fn(p + new Vector2(0, s)) - fn(p - new Vector2(0, s))) * sInv;
+
+                return new(dx, dy);
+            };
+        }
+
+        /// <summary>
+        /// Denoted ∇ · F(x,y) where F: (R, R) -> (R, R).
+        /// Computes the change in vector strength along the 2 axis.
+        /// </summary>
+        /// <remarks>
+        /// This is equivalent to ∂F.x/∂x + ∂F.y/∂y
+        /// </remarks>
+        /// <param name="s">The numerical precision of the derivatives</param>
+        /// <returns>f(x, y) where f: (R, R) -> R</returns>
+        public static ScalarFn Divergence ( VectorFn fn, float s = 0.001f ) {
+            var sInv = 1 / s / 2;
+
+            return p => {
+                var dx = (fn(p + new Vector2(s, 0)).X - fn(p - new Vector2(s, 0)).X) * sInv;
+                var dy = (fn(p + new Vector2(0, s)).Y - fn(p - new Vector2(0, s)).Y) * sInv;
+
+                return dx + dy;
+            };
+        }
+
+        /// <summary>
+        /// Denoted ∇ × F(x,y) where F: (R, R) -> (R, R).
+        /// Computes the rotation at a given point on a vector field.
+        /// </summary>
+        /// <remarks>
+        /// This is equivalent to ∂F.y/∂x - ∂F.x/∂y
+        /// </remarks>
+        /// <param name="s">The numerical precision of the derivatives</param>
+        /// <returns>f(x, y) where f: (R, R) -> R</returns>
+        public static ScalarFn Curl ( VectorFn fn, float s = 0.001f ) {
+            var sInv = 1 / s / 2;
+
+            return p => {
+                var dy_dx = (fn(p + new Vector2(s, 0)).Y - fn(p - new Vector2(s, 0)).Y) * sInv;
+                var dx_dy = (fn(p + new Vector2(0, s)).X - fn(p - new Vector2(0, s)).X) * sInv;
+
+                return dy_dx - dx_dy;
+            };
+        }
+
+        /// <summary>
+        /// Denoted (∇ · ∇, ∇^2 or Δ) f(x,y) where f: (R, R) -> R.
+        /// Computes the divergence of the gradient of a scalar field.
+        /// </summary>
+        /// <remarks>
+        /// This is equivalent to ∂^2f/∂x^2 + ∂^2f/∂y^2
+        /// </remarks>
+        /// <param name="s">The numerical precision of the derivatives</param>
+        /// <returns>f(x, y) where f: (R, R) -> R</returns>
+        public static ScalarFn Laplacian ( ScalarFn fn, float s = 0.001f ) {
+            var sInv = 1 / s;
+
+            return p => {
+                var k = fn(p);
+
+                var xp = (fn(p + new Vector2(s, 0)) - k) * sInv;
+                var xn = (k - fn(p - new Vector2(s, 0))) * sInv;
+                var dx = (xp - xn) * sInv;
+
+                var yp = (fn(p + new Vector2(0, s)) - k) * sInv;
+                var yn = (k - fn(p - new Vector2(0, s))) * sInv;
+                var dy = (yp - yn) * sInv;
+
+                return dx + dy;
+            };
+        }
+
+        /// <summary>
+        /// Denoted (∇ · ∇, ∇^2 or Δ) F(x,y) where F: (R, R) -> (R, R).
+        /// Computes divergence of the gradient of the components of the vector field.
+        /// </summary>
+        /// <remarks>
+        /// This is equivalent to (∇^2 F.x, ∇^2 F.y)
+        /// </remarks>
+        /// <param name="s">The numerical precision of the derivatives</param>
+        /// <returns>F(x, y) where F: (R, R) -> (R, R)</returns>
+        public static VectorFn Laplacian ( VectorFn fn, float s = 0.001f ) {
+            var sInv = 1 / s;
+
+            return p => {
+                var k = fn(p);
+
+                var xp = (fn(p + new Vector2(s, 0)) - k) * sInv;
+                var xn = (k - fn(p - new Vector2(s, 0))) * sInv;
+                var dx = (xp - xn) * sInv;
+
+                var yp = (fn(p + new Vector2(0, s)) - k) * sInv;
+                var yn = (k - fn(p - new Vector2(0, s))) * sInv;
+                var dy = (yp - yn) * sInv;
+
+                return dx + dy;
+            };
+        }
+
+        public static ScalarFn KernelBell ( float h = 1 ) {
+            var factor = 315f / 64 / MathF.PI / MathF.Pow( h, 6 );
+            var h2 = h * h;
+
+            return p => {
+                var r2 = p.LengthSquared;
+
+                return r2 <= h2
+                    ? factor * MathF.Pow( h2 - r2, 3 )
+                    : 0;
+            };
+        }
+        public static ScalarFn KernelSpike ( float h = 1 ) {
+            var factor = 15f / MathF.PI / MathF.Pow(h, 6);
+
+            return p => {
+                var r = p.Length;
+
+                return r <= h
+                    ? factor * MathF.Pow( h - r, 3 )
+                    : 0;
+            };
+        }
+        public static ScalarFn KernelAsymptote ( float h = 1 ) {
+            var h2 = h * h;
+            var h3 = h2 * h;
+            var factor = 15f / 2 / MathF.PI / h3;
+
+            return p => {
+                var r = p.Length;
+                var r2 = r * r;
+                var r3 = r * r2;
+
+                return r <= h
+                    ? factor * ( -r3 / 2 / h3 + r2 / h2 + h / 2 / r - 1 )
+                    : 0;
+            };
+        }
+    }
+
+    public static class ParticleField<T> where T : IHasPosition {
+        public static float FieldAt ( Vector2 position, IEnumerable<T> particles, Func<T, float> selector, Field.ScalarFn kernel, float scale = 1 ) {
+            position /= scale;
+
+            float value = 0;
+            foreach ( var particle in particles ) {
+                value += selector( particle ) * kernel( position - particle.Position / scale );
+            }
+
+            return value;
+        }
+        public static Vector2 FieldAt ( Vector2 position, IEnumerable<T> particles, Func<T, Vector2> selector, Field.ScalarFn kernel, float scale = 1 ) {
+            position /= scale;
+
+            Vector2 value = Vector2.Zero;
+            foreach ( var particle in particles ) {
+                value += selector( particle ) * kernel( position - particle.Position / scale );
+            }
+
+            return value;
+        }
+        public static Vector2 FieldAt ( Vector2 position, IEnumerable<T> particles, Func<T, float> selector, Field.VectorFn kernel, float scale = 1 ) {
+            position /= scale;
+
+            Vector2 value = Vector2.Zero;
+            foreach ( var particle in particles ) {
+                value += selector( particle ) * kernel( position - particle.Position / scale );
+            }
+
+            return value;
+        }
+    }
+}
