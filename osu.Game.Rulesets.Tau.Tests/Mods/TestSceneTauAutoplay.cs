f7f88d8e1f8e438f6d2b69e59b0ae719e010b2e6
naoei <altenyt@gmail.com>
2022-03-31 17:39:09 -0400

    Add relax and autoplay test scenes

---
+++ osu.Game.Rulesets.Tau.Tests/Mods/TestSceneTauAutoplay.cs
@@ -0,0 +1,16 @@
+﻿using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Tau.Mods;
+using osu.Game.Tests.Visual;
+
+namespace osu.Game.Rulesets.Tau.Tests.Mods
+{
+    public class TestSceneTauAutoplay : TestSceneOsuPlayer
+    {
+        protected override TestPlayer CreatePlayer(Ruleset ruleset)
+        {
+            SelectedMods.Value = new Mod[] { new TauModAutoplay() };
+
+            return base.CreatePlayer(ruleset);
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Tau.Tests/Mods/TestSceneTauRelax.cs
@@ -0,0 +1,16 @@
+﻿using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Tau.Mods;
+using osu.Game.Tests.Visual;
+
+namespace osu.Game.Rulesets.Tau.Tests.Mods
+{
+    public class TestSceneTauRelax : TestSceneOsuPlayer
+    {
+        protected override TestPlayer CreatePlayer(Ruleset ruleset)
+        {
+            SelectedMods.Value = new Mod[] { new TauModRelax() };
+
+            return base.CreatePlayer(ruleset);
+        }
+    }
+}
