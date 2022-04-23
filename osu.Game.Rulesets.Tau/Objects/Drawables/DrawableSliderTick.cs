4dbfc6962ac1e8b276e16676d609a22a2755603b
Coppertine <37494321+Coppertine@users.noreply.github.com>
2022-04-24 03:42:55 +1000

    Remove all visual, which also removed all sound at the same time.

--- osu.Game.Rulesets.Tau/Objects/Drawables/DrawableSliderTick.cs
+++ osu.Game.Rulesets.Tau/Objects/Drawables/DrawableSliderTick.cs
@@ -2,14 +2,9 @@
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
-using osu.Game.Graphics;
-using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Scoring;
-using osu.Game.Rulesets.Tau.Configuration;
 using osu.Game.Rulesets.Tau.UI;
 using osuTK;
-using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Tau.Objects.Drawables
 {
@@ -17,16 +12,9 @@ namespace osu.Game.Rulesets.Tau.Objects.Drawables
     {
         public DrawableSlider DrawableSlider => (DrawableSlider)ParentHitObject;
 
-        public Drawable DrawableCircle;
-
         private readonly BindableFloat size = new(12f);
 
         public override bool DisplayResult => false;
-
-        public DrawableSliderTick()
-            : this(null)
-        {
-        }
 
         public DrawableSliderTick(SliderTick hitObject)
             : base(hitObject)
@@ -37,30 +25,15 @@ namespace osu.Game.Rulesets.Tau.Objects.Drawables
             RelativeSizeAxes = Axes.Both;
             Size = Vector2.One;
 
-            AddInternal(DrawableCircle = new Container
+            AddInternal(new Container
             {
                 RelativePositionAxes = Axes.Both,
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
-                Alpha = 0,
                 AlwaysPresent = true,
                 Size = new Vector2(size.Default),
                 Children = new Drawable[]
                 {
-                    new Circle
-                    {
-                        Anchor = Anchor.Centre,
-                        Origin = Anchor.Centre,
-                        RelativeSizeAxes = Axes.Both
-                    },
-                    new Circle
-                    {
-                        Anchor = Anchor.Centre,
-                        Origin = Anchor.Centre,
-                        Colour = Color4.Black,
-                        RelativeSizeAxes = Axes.Both,
-                        Size = Vector2.Multiply(Size, 0.5f)
-                    }
                 }
             });
 
@@ -84,66 +57,10 @@ namespace osu.Game.Rulesets.Tau.Objects.Drawables
         [Resolved(canBeNull: true)]
         private TauCachedProperties properties { get; set; }
 
-        protected override void UpdateInitialTransforms()
-        {
-            base.UpdateInitialTransforms();
-
-            DrawableCircle.FadeIn(HitObject.TimeFadeIn);
-
-            if (properties != null && properties.InverseModEnabled.Value)
-                DrawableCircle.MoveToY(-1.0f);
-
-            DrawableCircle.MoveToY(-0.5f, HitObject.TimePreempt);
-        }
-
-        [BackgroundDependencyLoader(true)]
-        private void load(TauRulesetConfigManager config)
-        {
-            config?.BindWith(TauRulesetSettings.NotesSize, size);
-            size.BindValueChanged(value => DrawableCircle.Size = new Vector2(value.NewValue), true);
-        }
-
         protected override void CheckForResult(bool userTriggered, double timeOffset)
         {
             if (HitObject.StartTime <= Time.Current)
                 ApplyResult(r => r.Type = DrawableSlider.Tracking.Value ? HitResult.Great : HitResult.Miss);
-        }
-
-        [Resolved]
-        private OsuColour colour { get; set; }
-
-        protected override void UpdateHitStateTransforms(ArmedState state)
-        {
-            base.UpdateHitStateTransforms(state);
-
-            const double time_fade_hit = 250, time_fade_miss = 400;
-            var offset = new Vector2(0, -.1f);
-
-            if (properties != null && properties.InverseModEnabled.Value)
-                offset.Y = -offset.Y;
-
-            switch (state)
-            {
-                case ArmedState.Hit:
-                    DrawableCircle.ScaleTo(2f, time_fade_hit, Easing.OutQuint)
-                                  .FadeColour(colour.ForHitResult(Result.Type), time_fade_hit, Easing.OutQuint)
-                                  .MoveToOffset(offset, time_fade_hit, Easing.OutQuint)
-                                  .FadeOut(time_fade_hit);
-
-                    this.Delay(time_fade_hit).Expire();
-
-                    break;
-
-                case ArmedState.Miss:
-                    DrawableCircle.ScaleTo(0.5f, time_fade_miss, Easing.InQuint)
-                                  .FadeColour(Color4.Red, time_fade_miss, Easing.OutQuint)
-                                  .MoveToOffset(offset, time_fade_miss, Easing.OutQuint)
-                                  .FadeOut(time_fade_miss);
-
-                    this.Delay(time_fade_miss).Expire();
-
-                    break;
-            }
         }
     }
 }
