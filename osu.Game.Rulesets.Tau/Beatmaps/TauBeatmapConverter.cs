9a12d54b1dda611c8413791cb7410cc9ff986d2f
naoei <altenyt@gmail.com>
2022-04-23 17:01:38 -0400

    Properly colour hit lighting

--- osu.Game.Rulesets.Tau/Beatmaps/TauBeatmapConverter.cs
+++ osu.Game.Rulesets.Tau/Beatmaps/TauBeatmapConverter.cs
@@ -31,15 +31,16 @@ namespace osu.Game.Rulesets.Tau.Beatmaps
         protected override IEnumerable<TauHitObject> ConvertHitObject(HitObject original, IBeatmap beatmap, CancellationToken cancellationToken)
         {
             bool isHard = (original is IHasPathWithRepeats tmp ? tmp.NodeSamples[0] : original.Samples).Any(s => s.Name == HitSampleInfo.HIT_FINISH);
+            var comboData = original as IHasCombo;
 
             return original switch
             {
-                IHasPathWithRepeats path => convertToSlider(original, path, isHard, beatmap.Difficulty).Yield(),
-                _ => isHard && CanConvertToHardBeats ? convertToHardBeat(original).Yield() : convertToBeat(original).Yield()
+                IHasPathWithRepeats path => convertToSlider(original, comboData, path, isHard, beatmap.Difficulty).Yield(),
+                _ => isHard && CanConvertToHardBeats ? convertToHardBeat(original, comboData).Yield() : convertToBeat(original, comboData).Yield()
             };
         }
 
-        private TauHitObject convertToBeat(HitObject original)
+        private TauHitObject convertToBeat(HitObject original, IHasCombo comboData)
         {
             float angle = original switch
             {
@@ -54,21 +55,25 @@ namespace osu.Game.Rulesets.Tau.Beatmaps
             {
                 Samples = original.Samples,
                 StartTime = original.StartTime,
-                Angle = angle
+                Angle = angle,
+                NewCombo = comboData?.NewCombo ?? false,
+                ComboOffset = comboData?.ComboOffset ?? 0,
             };
         }
 
-        private TauHitObject convertToHardBeat(HitObject original) =>
+        private TauHitObject convertToHardBeat(HitObject original, IHasCombo comboData) =>
             new HardBeat
             {
                 Samples = original.Samples,
                 StartTime = original.StartTime,
+                NewCombo = comboData?.NewCombo ?? false,
+                ComboOffset = comboData?.ComboOffset ?? 0,
             };
 
-        private TauHitObject convertToSlider(HitObject original, IHasPathWithRepeats data, bool isHard, IBeatmapDifficultyInfo info)
+        private TauHitObject convertToSlider(HitObject original, IHasCombo comboData, IHasPathWithRepeats data, bool isHard, IBeatmapDifficultyInfo info)
         {
             TauHitObject convertBeat()
-                => CanConvertToHardBeats && isHard ? convertToHardBeat(original) : convertToBeat(original);
+                => CanConvertToHardBeats && isHard ? convertToHardBeat(original, comboData) : convertToBeat(original, comboData);
 
             if (!CanConvertToSliders)
                 return convertBeat();
@@ -117,7 +122,9 @@ namespace osu.Game.Rulesets.Tau.Beatmaps
                 NodeSamples = data.NodeSamples,
                 RepeatCount = data.RepeatCount,
                 Angle = firstAngle,
-                Path = new PolarSliderPath(nodes.ToArray())
+                Path = new PolarSliderPath(nodes.ToArray()),
+                NewCombo = comboData?.NewCombo ?? false,
+                ComboOffset = comboData?.ComboOffset ?? 0,
             };
         }
     }
--- osu.Game.Rulesets.Tau/Objects/TauHitObject.cs
+++ osu.Game.Rulesets.Tau/Objects/TauHitObject.cs
@@ -1,14 +1,16 @@
-﻿using osu.Game.Beatmaps;
+﻿using osu.Framework.Bindables;
+using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Rulesets.Judgements;
 using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Rulesets.Scoring;
 using osu.Game.Rulesets.Tau.Judgements;
 using osu.Game.Rulesets.Tau.Scoring;
 
 namespace osu.Game.Rulesets.Tau.Objects
 {
-    public class TauHitObject : HitObject
+    public class TauHitObject : HitObject, IHasComboInformation
     {
         public double TimePreempt = 600;
         public double TimeFadeIn = 100;
@@ -21,6 +23,48 @@ namespace osu.Game.Rulesets.Tau.Objects
             base.ApplyDefaultsToSelf(controlPointInfo, difficulty);
 
             TimePreempt = IBeatmapDifficultyInfo.DifficultyRange(difficulty.ApproachRate, 1800, 1200, 450);
+        }
+
+        public virtual bool NewCombo { get; set; }
+
+        public readonly Bindable<int> ComboOffsetBindable = new Bindable<int>();
+
+        public int ComboOffset
+        {
+            get => ComboOffsetBindable.Value;
+            set => ComboOffsetBindable.Value = value;
+        }
+
+        public Bindable<int> IndexInCurrentComboBindable { get; } = new Bindable<int>();
+
+        public virtual int IndexInCurrentCombo
+        {
+            get => IndexInCurrentComboBindable.Value;
+            set => IndexInCurrentComboBindable.Value = value;
+        }
+
+        public Bindable<int> ComboIndexBindable { get; } = new Bindable<int>();
+
+        public virtual int ComboIndex
+        {
+            get => ComboIndexBindable.Value;
+            set => ComboIndexBindable.Value = value;
+        }
+
+        public Bindable<int> ComboIndexWithOffsetsBindable { get; } = new Bindable<int>();
+
+        public int ComboIndexWithOffsets
+        {
+            get => ComboIndexWithOffsetsBindable.Value;
+            set => ComboIndexWithOffsetsBindable.Value = value;
+        }
+
+        public Bindable<bool> LastInComboBindable { get; } = new Bindable<bool>();
+
+        public bool LastInCombo
+        {
+            get => LastInComboBindable.Value;
+            set => LastInComboBindable.Value = value;
         }
     }
 }
