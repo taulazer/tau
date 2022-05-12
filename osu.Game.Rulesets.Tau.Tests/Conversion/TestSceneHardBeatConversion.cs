21f63834fe2c086b2daaaa5bdad701ca1aa75dcc
naoei <altenyt@gmail.com>
2022-05-12 15:17:03 -0400

    Hard beat conversion test

---
+++ osu.Game.Rulesets.Tau.Tests/Conversion/TestSceneHardBeatConversion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using osu.Game.Audio;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Tau.Objects;
+
+namespace osu.Game.Rulesets.Tau.Tests.Conversion
+{
+    public class TestSceneHardBeatConversion : TauConversionTestScene
+    {
+        protected override IEnumerable<HitObject> CreateHitObjects()
+        {
+            yield return new HitObject { Samples = { new HitSampleInfo(HitSampleInfo.HIT_FINISH) } };
+        }
+
+        protected override void CreateAsserts(IEnumerable<HitObject> hitObjects)
+        {
+            AddAssert("Hitobjects are of correct type", () => hitObjects.Any(o => o is HardBeat));
+        }
+    }
+}
