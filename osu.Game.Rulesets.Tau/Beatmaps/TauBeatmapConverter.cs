e01415273f292b56bf2fed9172c3fae56e0e691e
naoei <altenyt@gmail.com>
2022-05-12 15:12:53 -0400

    Move `GetHitObjectAngle()` and create constants in beatmap converter

--- osu.Game.Rulesets.Tau/Beatmaps/TauBeatmapConverter.cs
+++ osu.Game.Rulesets.Tau/Beatmaps/TauBeatmapConverter.cs
@@ -9,6 +9,7 @@ using osu.Game.Beatmaps.Legacy;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Rulesets.Tau.Objects;
+using osuTK;
 
 namespace osu.Game.Rulesets.Tau.Beatmaps
 {
@@ -23,6 +24,9 @@ namespace osu.Game.Rulesets.Tau.Beatmaps
         public bool CanConvertToSliders { get; set; } = true;
         public bool CanConvertImpossibleSliders { get; set; }
         public int SliderDivisor { get; set; } = 4;
+
+        public static readonly Vector2 STANDARD_PLAYFIELD_SIZE = new(512, 384);
+        public static readonly Vector2 STANDARD_PLAYFIELD_CENTER = STANDARD_PLAYFIELD_SIZE / 2;
 
         public TauBeatmapConverter(Ruleset ruleset, IBeatmap beatmap)
             : base(beatmap, ruleset)
@@ -56,8 +60,8 @@ namespace osu.Game.Rulesets.Tau.Beatmaps
             => original switch
             {
                 IHasPosition pos => pos.Position.GetHitObjectAngle(),
-                IHasXPosition xPos => xPos.X.Remap(0, 512, 0, 360),
-                IHasYPosition yPos => yPos.Y.Remap(0, 384, 0, 360),
+                IHasXPosition xPos => xPos.X.Remap(0, STANDARD_PLAYFIELD_SIZE.X, 0, 360),
+                IHasYPosition yPos => yPos.Y.Remap(0, STANDARD_PLAYFIELD_SIZE.Y, 0, 360),
                 IHasAngle ang => ang.Angle,
                 _ => 0
             };
@@ -196,5 +200,16 @@ namespace osu.Game.Rulesets.Tau.Beatmaps
                                              : 4
             };
         }
+    }
+
+    public static class TauBeatmapConverterExtensions
+    {
+        /// <summary>
+        /// Gets the theta angle from the playfield's center.
+        /// Note that this only works for legacy sizing (512x384)
+        /// </summary>
+        /// <param name="target">The target <see cref="HitObject"/> position.</param>
+        public static float GetHitObjectAngle(this Vector2 target)
+            => TauBeatmapConverter.STANDARD_PLAYFIELD_CENTER.GetDegreesFromPosition(target);
     }
 }
