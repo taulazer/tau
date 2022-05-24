96bfd0d64a3516ed04aa77be1298940ed50eb375
naoei <altenyt@gmail.com>
2022-05-24 12:00:54 -0400

    Clean aim skill

--- osu.Game.Rulesets.Tau/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Tau/Difficulty/Skills/Aim.cs
@@ -12,7 +12,7 @@ namespace osu.Game.Rulesets.Tau.Difficulty.Skills
     {
         private Type[] AllowedObjectTypes;
 
-        protected override int HistoryLength => 2;
+        protected override int HistoryLength => 10;
         protected override double SkillMultiplier => 50;
         private const double slider_multiplier = 1.5;
         protected override double StrainDecayBase => 0.2;
@@ -23,46 +23,44 @@ namespace osu.Game.Rulesets.Tau.Difficulty.Skills
             AllowedObjectTypes = allowedObjectTypes;
         }
 
+        private double calculateStrain(TauAngledDifficultyHitObject current, TauAngledDifficultyHitObject last)
+        {
+            double velocity = current.Distance / current.StrainTime;
+
+            if (!AllowedObjectTypes.Any(t => t == typeof(Slider) && last.BaseObject is Slider))
+                return velocity;
+
+            // Slider calculation
+
+            if (!(last.TravelDistance >= current.AngleRange))
+                return velocity;
+
+            double travelVelocity = last.LazyTravelDistance / last.TravelTime;
+            double movementVelocity = current.Distance / current.StrainTime;
+
+            velocity = Math.Max(velocity, movementVelocity + travelVelocity);
+            velocity += (last.TravelDistance / last.TravelTime) * slider_multiplier;
+
+            return velocity;
+        }
+
         protected override double StrainValueOf(DifficultyHitObject current)
         {
-            if (Previous.Count <= 1)
+            if (Previous.Count <= 1 || current is not TauAngledDifficultyHitObject)
                 return 0;
 
-            if (current is not TauAngledDifficultyHitObject || Previous[0] is not TauAngledDifficultyHitObject)
+            // Get the last available angled hit object in the history.
+            var prevAngled = Previous.Where(p => p is TauAngledDifficultyHitObject);
+            if (!prevAngled.Any())
                 return 0;
 
             var tauCurrObj = (TauAngledDifficultyHitObject)current;
-            var tauLastObj = (TauAngledDifficultyHitObject)Previous[0]; // TODO: "Last Object" should be the last available angled hit object.
+            var tauLastObj = (TauAngledDifficultyHitObject)prevAngled.First();
 
-            if (tauCurrObj.Distance == 0)
+            if (tauCurrObj.Distance == 0 || !(tauCurrObj.Distance >= tauCurrObj.AngleRange))
                 return 0;
 
-            if (!(tauCurrObj.Distance >= tauCurrObj.AngleRange)) return 0;
-
-            double currVelocity = tauCurrObj.Distance / tauCurrObj.StrainTime;
-
-            if (AllowedObjectTypes.Any(t => t == typeof(Slider)) && tauLastObj.BaseObject is Slider && tauLastObj.TravelDistance < tauCurrObj.AngleRange)
-            {
-                double travelVelocity = tauLastObj.LazyTravelDistance / tauLastObj.TravelTime; // calculate the slider velocity from slider head to slider end.
-                double movementVelocity = tauCurrObj.Distance / tauCurrObj.StrainTime; // calculate the movement velocity from slider end to current object
-
-                currVelocity = Math.Max(currVelocity, movementVelocity + travelVelocity); // take the larger total combined velocity.
-            }
-
-            double sliderBonus = 0;
-            double aimStrain = currVelocity;
-
-            // Sliders should be treated as beats if their travel distance is short enough.
-            if (tauLastObj.TravelTime != 0 && tauLastObj.TravelDistance >= tauCurrObj.AngleRange)
-            {
-                // Reward sliders based on velocity.
-                sliderBonus = tauLastObj.TravelDistance / tauLastObj.TravelTime;
-            }
-
-            // Add in additional slider velocity bonus.
-            if (AllowedObjectTypes.Any(t => t == typeof(Slider)))
-                aimStrain += sliderBonus * slider_multiplier;
-            return aimStrain;
+            return calculateStrain(tauCurrObj, tauLastObj);
         }
 
         #region PP Calculation
@@ -72,9 +70,7 @@ namespace osu.Game.Rulesets.Tau.Difficulty.Skills
             double rawAim = context.DifficultyAttributes.AimDifficulty;
             double aimValue = Math.Pow(5.0 * Math.Max(1.0, rawAim / 0.0675) - 4.0, 3.0) / 100000.0; // TODO: Figure values here.
 
-            double lengthBonus = 0.95 +
-                                 0.4 * Math.Min(1.0, context.TotalHits / 2000.0) +
-                                 (context.TotalHits > 2000 ? Math.Log10(context.TotalHits / 2000.0) * 0.5 : 0.0); // TODO: Figure values here.
+            double lengthBonus = 0.95 + 0.4 * Math.Min(1.0, context.TotalHits / 2000.0) + (context.TotalHits > 2000 ? Math.Log10(context.TotalHits / 2000.0) * 0.5 : 0.0); // TODO: Figure values here.
             aimValue *= lengthBonus;
             TauDifficultyAttributes attributes = context.DifficultyAttributes;
 
@@ -88,8 +84,7 @@ namespace osu.Game.Rulesets.Tau.Difficulty.Skills
 
             // Penalize misses by assessing # of misses relative to the total # of objects. Default a 3% reduction for any # of misses.
             if (context.EffectiveMissCount > 0)
-                aimValue *= 0.97 *
-                            Math.Pow(1 - Math.Pow(context.EffectiveMissCount / context.TotalHits, 0.775), context.EffectiveMissCount); // TODO: Figure values here.
+                aimValue *= 0.97 * Math.Pow(1 - Math.Pow(context.EffectiveMissCount / context.TotalHits, 0.775), context.EffectiveMissCount); // TODO: Figure values here.
 
             // We assume 15% of sliders in a map are difficult since there's no way to tell from the performance calculator.
             double estimateDifficultSliders = attributes.SliderCount * 0.15;
@@ -98,8 +93,7 @@ namespace osu.Game.Rulesets.Tau.Difficulty.Skills
             {
                 double estimateSliderEndsDropped = Math.Clamp(Math.Min(context.CountOk + context.CountMiss, attributes.MaxCombo - context.ScoreMaxCombo), 0,
                     estimateDifficultSliders);
-                double sliderNerfFactor = (1 - attributes.SliderFactor) * Math.Pow(1 - estimateSliderEndsDropped / estimateDifficultSliders, 3) +
-                                          attributes.SliderFactor;
+                double sliderNerfFactor = (1 - attributes.SliderFactor) * Math.Pow(1 - estimateSliderEndsDropped / estimateDifficultSliders, 3) + attributes.SliderFactor;
                 aimValue *= sliderNerfFactor;
             }
 
