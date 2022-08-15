104b0b2610479f14c7b5335dc7af8c1dfe88c6ba
Coppertine <37494321+Coppertine@users.noreply.github.com>
2022-08-15 22:21:14 +1000

    Add Acronym and descriptions to FO and FI

--- osu.Game.Rulesets.Tau/Mods/TauModFadeOut.cs
+++ osu.Game.Rulesets.Tau/Mods/TauModFadeOut.cs
@@ -5,6 +5,10 @@ namespace osu.Game.Rulesets.Tau.Mods
     public class TauModFadeOut : TauModHidden
     {
         public override IconUsage? Icon => TauIcons.ModFadeOut;
+        public override string Acronym => "FO";
+
+        // Modification from osu!mania's description of Hidden mod.
+        public override string Description => "Beats fade out before you hit them!";
         protected override MaskingMode Mode => MaskingMode.FadeOut;
         protected override float InitialCoverage => 0.4f;
     }
