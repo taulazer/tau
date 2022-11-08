4266cbab34b4c418dd66bfdd475df041d820e662
Nora <altenyt@gmail.com>
2022-11-08 10:19:36 -0500

    Create initial test cases for difficulty attributes

---
+++ osu.Game.Rulesets.Tau.Tests/NonVisual/AimCalculationTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Tau.Difficulty;
+using osu.Game.Rulesets.Tau.Mods;
+using osu.Game.Rulesets.Tau.Objects;
+using osu.Game.Tests.Beatmaps;
+
+namespace osu.Game.Rulesets.Tau.Tests.NonVisual;
+
+public class AimCalculationTests : CalculationTestScene
+{
+    public override WorkingBeatmap CreateBeatmap()
+    {
+        // TODO: Fetch from resource instead?
+        var beatmap = new Beatmap
+        {
+            HitObjects = new List<HitObject>
+            {
+                createBeatObject(0, 0),
+                createBeatObject(1000, 0),
+                createBeatObject(1500, 90),
+                createBeatObject(1750, 180),
+            }
+        };
+
+        return new TestWorkingBeatmap(beatmap);
+    }
+
+    public override void AssertDifficulty(IEnumerable<Mod> mods, TauDifficultyAttributes attributes)
+    {
+        var modsArray = mods.ToArray();
+
+        if (modsArray.Length == 0)
+        {
+            Assert.AreEqual(attributes.StarRating, 0.54396336135843126);
+            return;
+        }
+
+        if (ContainMods(modsArray, new Mod[] { new TauModDoubleTime(), new TauModHardRock() }))
+        {
+            Assert.AreEqual(attributes.StarRating, 0.70501975613138956);
+            return;
+        }
+    }
+
+    public override void AssertPerformance(IEnumerable<Mod> mods, TauDifficultyAttributes attributes)
+    {
+    }
+
+    private Beat createBeatObject(double time, float angle)
+    {
+        return new Beat
+        {
+            Angle = angle,
+            StartTime = time
+        };
+    }
+}
---
+++ osu.Game.Rulesets.Tau.Tests/NonVisual/CalculationTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Tau.Difficulty;
+using osu.Game.Rulesets.Tau.Mods;
+using osu.Game.Tests.Visual;
+
+namespace osu.Game.Rulesets.Tau.Tests.NonVisual;
+
+public abstract class CalculationTestScene : OsuTestScene
+{
+    public abstract WorkingBeatmap CreateBeatmap();
+
+    public abstract void AssertDifficulty(IEnumerable<Mod> mods, TauDifficultyAttributes attributes);
+
+    public abstract void AssertPerformance(IEnumerable<Mod> mods, TauDifficultyAttributes attributes);
+
+    private WorkingBeatmap beatmap;
+    private TauDifficultyCalculator diffCalculator;
+
+    [SetUp]
+    public void Setup()
+    {
+        beatmap = CreateBeatmap();
+        diffCalculator = new TauDifficultyCalculator(new TauRuleset().RulesetInfo, beatmap);
+    }
+
+    [Test]
+    public void TestDifficulty()
+    {
+        assertDifficulty();
+        assertDifficulty(new Mod[] { new TauModDoubleTime(), new TauModHardRock() });
+    }
+
+    [Test]
+    public void TestPerformance()
+    {
+        assertPerformance();
+        assertPerformance(new Mod[] { new TauModDoubleTime(), new TauModHardRock() });
+    }
+
+    private void assertDifficulty(IEnumerable<Mod> mods = null)
+    {
+        mods ??= Array.Empty<Mod>();
+        AssertDifficulty(mods, diffCalculator.Calculate(mods) as TauDifficultyAttributes);
+    }
+
+    private void assertPerformance(IEnumerable<Mod> mods = null)
+    {
+        mods ??= Array.Empty<Mod>();
+        AssertPerformance(mods, diffCalculator.Calculate(mods) as TauDifficultyAttributes);
+    }
+
+    protected static bool ContainMods(IReadOnlyList<Mod> mods, IEnumerable<Mod> toCheck)
+        => toCheck.All(mod => mods.Any(m => m.GetType() == mod.GetType()));
+}
