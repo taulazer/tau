6527250459108902d5a88f3527d52186918983e8
Tenexxt <tenexxt2009@gmail.com>
2026-03-08 23:36:00 +0100

    Chnage `TauBarrelRoll` to ommit useless stuff
    Co-Authored-By: Derrick Timmermans <derrick.timmermans@outlook.com>

--- osu.Game.Rulesets.Tau/Mods/TauModBarrelRoll.cs
+++ osu.Game.Rulesets.Tau/Mods/TauModBarrelRoll.cs
@@ -1,18 +1,7 @@
-
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Tau.Objects;
-using osu.Game.Rulesets.Tau.UI;
-using osu.Game.Rulesets.UI;
-using osuTK;
 
 namespace osu.Game.Rulesets.Tau.Mods
 {
-    public class TauModBarrelRoll : ModBarrelRoll<TauHitObject>{
-        public override void Update(Playfield playfield)
-        {
-            base.Update(playfield);
-            TauCursor cursor = (TauCursor)playfield.Cursor;
-            cursor.ActiveCursor.Rotation =+ 167f;
-        }
-    }
+    public class TauModBarrelRoll : ModBarrelRoll<TauHitObject>;
 }
